13f039c82fff83394abe35a66e7cc1f58a2078df
hodol <wdf3412@gmail.com>
2019-05-03 23:36:00 +0900

    Change default values, and refactor for loop.

--- Test/src/ca/demo/salestool/SalesData.java
+++ Test/src/ca/demo/salestool/SalesData.java
@@ -1,9 +1,12 @@
 package ca.demo.salestool;
 
 public class SalesData {
-	private int data[] = {0, 4, 42};
+	private int data[] = {-1, 0, 4, 42, 84};
 	
 	public void display() {
-		System.out.println("SalesData클라스 display메서드");		
+		System.out.println("SalesData클라스 수정했다고요 display메서드");	
+		for(int i:data) {
+			System.out.println("Next Value: "+data[i]);
+		}
 	}
 }
