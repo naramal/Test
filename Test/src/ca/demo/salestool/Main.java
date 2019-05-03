b9bad472a5a2b3d12d525f58af59fac68d35b22b
hodol <wdf3412@gmail.com>
2019-05-03 23:01:51 +0900

    Initial commit of project.

---
+++ Test/src/ca/demo/salestool/Main.java
@@ -0,0 +1,15 @@
+package ca.demo.salestool;
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+		
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("안녕하세여");
+		System.out.println("세일즈 데이터 에여");
+	}
+}
---
+++ Test/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,9 @@
+package ca.demo.salestool;
+
+public class SalesData {
+	private int data[] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("SalesData클라스 display메서드");		
+	}
+}
